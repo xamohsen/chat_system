9938fa6a0c4657c31718ddb9edfc88f3cc5292ab
Ahmed Mohsen <xa.mohsen@gmail.com>
2019-07-14 21:41:10 +0200

    Add more scenarios to the app creation APIs.

--- spec/requests/chat_app_controller_spec.rb
+++ spec/requests/chat_app_controller_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe 'chat app api', type: :request do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
-    it 'should not return the app id' do
-      expect(json.first()['id']).to eq(nil)
+    it 'should not return the apps id' do
+      json.each {|app| expect(app['id']).to eq(nil)}
     end
   end
 
@@ -40,35 +40,41 @@ RSpec.describe 'chat app api', type: :request do
   describe "Post /application/" do
 
     context 'when the request is valid' do
-      before {post "/application/", params: {app: {name: 'app'}}}
+      before {post "/application/", params: {app: {name: 'app#1'}}}
       it 'returns status code 200' do
         expect(response).to have_http_status(201)
       end
-      it 'returns chat_app by token' do
-        expect(json['token']).to eq(11)
+      it 'returns chat_app name and token +1 after creation' do
+        expect(json['token']).to eq(apps_number + 1)
+        expect(json['name']).to eq('app#1')
+      end
+      it 'returns chat_app name and token +2  after creating another app' do
+        post "/application/", params: {app: {name: 'app#2'}}
+        expect(json['token']).to eq(apps_number + 2)
+        expect(json['name']).to eq('app#2')
+      end
+      it 'should not return the app id' do
+        expect(json['id']).to eq(nil)
       end
     end
 
     context 'when the request is invalid' do
       before {post "/application/", params: {app: {}}}
-
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
       it 'returns a validation failure message' do
-        expect(response.body)
-            .to match("Name can't be blank")
+        expect(response.body).to match("Name can't be blank")
       end
-
       it 'returns error request#1' do
         post "/application/"
-        expect(response.body)
-            .to match("Validation failed: Name can't be blank, Token can't be blank")
+        expect(response.body).to match("Validation failed: Name can't be blank, Token can't be blank")
+        expect(response).to have_http_status(422)
       end
       it 'returns error request#2' do
         post "/application/", params: {}
-        expect(response.body)
-            .to match("Validation failed: Name can't be blank, Token can't be blank")
+        expect(response.body).to match("Validation failed: Name can't be blank, Token can't be blank")
+        expect(response).to have_http_status(422)
       end
     end
 
