d74209c4294b7bfb8cf47479f67125747c373b34
Ahmed Mohsen <xa.mohsen@gmail.com>
2019-07-17 04:34:37 +0200

    delete un-used file

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /chat_system/tmp/pids/server.pid
-# Start the server
-bundle exec rails server
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
